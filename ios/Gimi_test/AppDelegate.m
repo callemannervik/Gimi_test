e1f420d11aa2a69965d33c513f33ec2db607282a
Calle Mannervik <CalleMannervik@Calles-MacBook-Pro-2.local>
2020-07-20 18:06:44 +0200

    iOS fixes

--- ios/Gimi_test/AppDelegate.m
+++ ios/Gimi_test/AppDelegate.m
@@ -36,7 +36,7 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"Gimi_test"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:0.803f blue:0.725f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
